1e0520c7018f49eef1bf609e3d0854fd6ed25690
BogdanCucura2021 <94701310+BogdanCucura2021@users.noreply.github.com>
2021-11-22 17:35:53 +0100

    Comunicacioseriebasica ex 4

---
+++ Comunicació Sèrie Bàsica/ComunicacioSerieBasic_ex4/ComunicacioSerieBasic_ex4.ino
@@ -0,0 +1,38 @@
+/**************************************************************************
+**                                                                       **
+**                                                                       **
+**                                TÍTOL:                                 **
+**                                                                       **
+**                                                                       **
+**                                                                       **
+**                                                                       **
+**NOM: Bogdan Laurentiu Cucura                            DATA:          **
+**************************************************************************/
+
+//****************************INCLUDE**************************************
+
+
+
+//****************************VARIABLE*************************************
+
+
+
+//****************************SETUP****************************************
+
+void setup() {                 //configura el final de salida 
+Serial.begin(9600);
+Serial.print("Escull el numero de l'operacio que vols realitzar?");
+delay(500);
+Serial.println(" 1.Comprovar numero de tarjeta de credit");
+delay(500);
+Serial.println(" 2.Comprovar numero de compte bancari");
+delay(250);
+Serial.println(" 3.Buscar un digit perdut de tarjeta de credit");
+} 
+
+//****************************LOOP*****************************************
+void loop() {                 //configura el bucle 
+
+}
+
+//****************************FUNCIONS*************************************
