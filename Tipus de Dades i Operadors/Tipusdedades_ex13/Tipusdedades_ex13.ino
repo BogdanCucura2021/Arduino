1abb139e8a67c6664dfb37e5744f55170a78e0f9
BogdanCucura2021 <94701310+BogdanCucura2021@users.noreply.github.com>
2021-11-29 15:49:29 +0100

    Update Tipusdedades_ex13.ino

--- Tipus de Dades i Operadors/Tipusdedades_ex13/Tipusdedades_ex13.ino
+++ Tipus de Dades i Operadors/Tipusdedades_ex13/Tipusdedades_ex13.ino
@@ -22,17 +22,17 @@ float d;
 //****************************SETUP****************************************
 
 void setup() {                 //configura el final de salida 
-Serial.begin(9600);
-Serial.println("Here is division: ");
-Serial.print("a = ");
-Serial.println(a);
-Serial.print("b = ");
-Serial.println(b);
-
-d = a / b;
-
-Serial.print("a / b = ");
-Serial.println(d);
+  Serial.begin(9600);
+  Serial.println("Here is division: ");
+  Serial.print("a = ");
+  Serial.println(a);
+  Serial.print("b = ");
+  Serial.println(b);
+  
+  d = a / b;
+  
+  Serial.print("a / b = ");
+  Serial.println(d);
 } 
 
 //****************************LOOP*****************************************
