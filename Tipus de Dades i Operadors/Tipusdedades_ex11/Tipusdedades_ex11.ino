489f5431c39849ea09f91fbc97f6e3b6ecf7425b
BogdanCucura2021 <94701310+BogdanCucura2021@users.noreply.github.com>
2021-11-29 12:37:31 +0100

    Update Tipusdedades_ex11.ino

--- Tipus de Dades i Operadors/Tipusdedades_ex11/Tipusdedades_ex11.ino
+++ Tipus de Dades i Operadors/Tipusdedades_ex11/Tipusdedades_ex11.ino
@@ -34,12 +34,19 @@ drive_mb = drive_gb;
 drive_mb = drive_mb * 1024;
 
 Serial.print(drive_mb);
-Serial.print("Megabytes, ");
+Serial.print(" Megabytes, ");
 
 drive_kb = drive_mb * 1024;
 
 Serial.print(drive_kb);
 Serial.println(" Kilobytes.");
+Serial.print("But it really only stores ");
+
+real_drive_mb = drive_gb;
+real_drive_mb = real_drive_mb * 1000;
+
+Serial.print(real_drive_mb);
+
 } 
 
 //****************************LOOP*****************************************
