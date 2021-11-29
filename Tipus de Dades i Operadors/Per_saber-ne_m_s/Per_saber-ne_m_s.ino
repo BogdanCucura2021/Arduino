7c2b5556d9c6b3780ac810edfa709eabc1661eb5
BogdanCucura2021 <94701310+BogdanCucura2021@users.noreply.github.com>
2021-11-29 16:16:21 +0100

    Per saber-ne més

---
+++ Tipus de Dades i Operadors/Per_saber-ne_m_s/Per_saber-ne_m_s.ino
@@ -0,0 +1,75 @@
+/**************************************************************************
+**                                                                       **
+**                                                                       **
+**                                TÍTOL: Per sabr-ne més                 **
+**                                                                       **
+**                                                                       **
+**                                                                       **
+**                                                                       **
+**NOM: Bogdan Laurentiu Cucura                            DATA:29/11/2021**
+**************************************************************************/
+
+//****************************INCLUDE**************************************
+
+
+
+//****************************VARIABLE*************************************
+boolean A, B, C, Output;
+
+
+//****************************SETUP****************************************
+
+void setup() {                 //configura el final de salida 
+  Serial.begin(9600);
+
+  Serial.println("Taula de la Veritat");
+
+  Serial.println("  A  B  C  | Output");
+  Serial.println("___________|_______");
+
+  A=0; B=0; C=0;
+  Output = (~A & B & C) | (A & ~B & C) | (A & B & ~C) | (A & B & C);
+  Serial.print("  0  0  0  | ");
+  Serial.println(Output);
+  A=0; B=0; C=1;
+    Output = (~A & B & C) | (A & ~B & C) | (A & B & ~C) | (A & B & C);
+  Serial.print("  0  0  1  | ");
+  Serial.println(Output);
+
+    A=0; B=1; C=0;
+    Output = (~A & B & C) | (A & ~B & C) | (A & B & ~C) | (A & B & C);
+  Serial.print("  0  1  0  | ");
+  Serial.println(Output);
+
+     A=0; B=1; C=1;
+    Output = (~A & B & C) | (A & ~B & C) | (A & B & ~C) | (A & B & C);
+  Serial.print("  0  1  1  | ");
+  Serial.println(Output);
+
+      A=1; B=0; C=0;
+    Output = (~A & B & C) | (A & ~B & C) | (A & B & ~C) | (A & B & C);
+  Serial.print("  1  0  0  | ");
+  Serial.println(Output);
+
+        A=1; B=0; C=1;
+    Output = (~A & B & C) | (A & ~B & C) | (A & B & ~C) | (A & B & C);
+  Serial.print("  1  0  1  | ");
+  Serial.println(Output);
+
+          A=1; B=1; C=0;
+    Output = (~A & B & C) | (A & ~B & C) | (A & B & ~C) | (A & B & C);
+  Serial.print("  1  1  0  | ");
+  Serial.println(Output);
+
+            A=1; B=1; C=1;
+    Output = (~A & B & C) | (A & ~B & C) | (A & B & ~C) | (A & B & C);
+  Serial.print("  1  1  1  | ");
+  Serial.println(Output);
+} 
+
+//****************************LOOP*****************************************
+void loop() {                 //configura el bucle 
+  
+}
+
+//****************************FUNCIONS*************************************
