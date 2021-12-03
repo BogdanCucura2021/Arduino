50e4f224624e41bb3fb43ff4864ad8b9c2591c61
BogdanCucura2021 <94701310+BogdanCucura2021@users.noreply.github.com>
2021-12-03 15:53:18 +0100

    Update If_switch_ex5.ino

--- Estructures de Control de Selecció if & switch/If_switch_ex5/If_switch_ex5.ino
+++ Estructures de Control de Selecció if & switch/If_switch_ex5/If_switch_ex5.ino
@@ -29,39 +29,39 @@ Serial.begin(9600);
 void loop() {                 //configura el bucle 
   if ( Carboni < 3.5)
   {
-    Serial.println("A");
+    Serial.println("Rang-A");
     delay(1000); 
   }
  else if ( Carboni < 6.5 && Carboni >= 3.5)
   {
-    Serial.println("B");
+    Serial.println("Rang-B");
     delay(1000); 
   }
     else if ( Carboni < 11.1 && Carboni >= 6.5)
     {
-    Serial.println("C");
+    Serial.println("Rang-C");
     delay(1000);
     }
-      else if ( Carboni < 17.7 && Carboni >= 11.1)
-      {
-      Serial.println("D");
-      delay(1000);
-      }
-        else if (Carboni <38.2 && Carboni >= 17.7)
-        {
-          Serial.println("E");
-          delay(1000);
-        }
-          else if (Carboni <43.2 && Carboni >= 38.2)
-          {
-            Serial.println("F");
-            delay(1000); 
-          }
-          else
-          {
-            Serial.println("G");
-            delay(1000);
-          }
+    else if ( Carboni < 17.7 && Carboni >= 11.1)
+    {
+    Serial.println("Rang-D");
+    delay(1000);
+    }
+    else if (Carboni <38.2 && Carboni >= 17.7)
+    {
+    Serial.println("Rang-E");
+    delay(1000);
+    }
+    else if (Carboni <43.2 && Carboni >= 38.2)
+    {
+    Serial.println("Rang-F");
+    delay(1000); 
+    }
+    else
+    {
+    Serial.println("Rang-G");
+    delay(1000);
+    }
 }
 
 //****************************FUNCIONS*************************************
