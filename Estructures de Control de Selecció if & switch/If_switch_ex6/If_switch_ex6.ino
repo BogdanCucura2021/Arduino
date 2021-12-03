a5ab2e0bce78cf7ac5bda4dc2efcb9fdb36cb93e
BogdanCucura2021 <94701310+BogdanCucura2021@users.noreply.github.com>
2021-12-03 16:07:12 +0100

    Create If_switch_ex6.ino

---
+++ Estructures de Control de Selecció if & switch/If_switch_ex6/If_switch_ex6.ino
@@ -0,0 +1,51 @@
+/**************************************************************************
+**                                                                       **
+**                                                                       **
+**                                TÍTOL:                                 **
+**                                                                       **
+**                                                                       **
+**                                                                       **
+**                                                                       **
+**NOM: Bogdan Laurentiu Cucura                            DATA:          **
+**************************************************************************/
+
+//****************************INCLUDE**************************************
+
+
+
+//****************************VARIABLE*************************************
+
+int sensorReading = 4; 
+
+//****************************SETUP****************************************
+
+void setup() {                 //configura el final de salida 
+{
+  Serial.begin(9600);
+  Serial.print("The day is "); 
+   
+  switch (sensorReading) {
+  case 0:    
+    Serial.println("dark");
+    break;
+  case 1:    
+    Serial.println("dim");
+    break;
+  case 2:    
+    Serial.println("medium");
+    break;
+  case 3:
+    Serial.println("bright");
+    break;
+  default:
+    Serial.println("... I don't know!!!");
+  } 
+}
+} 
+
+//****************************LOOP*****************************************
+void loop() {                 //configura el bucle 
+  
+}
+
+//****************************FUNCIONS*************************************
