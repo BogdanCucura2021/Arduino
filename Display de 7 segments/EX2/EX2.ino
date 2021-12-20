88c4e4d1e871cab8a347a7b33739c8a47166e7a2
BogdanCucura2021 <94701310+BogdanCucura2021@users.noreply.github.com>
2021-12-20 16:20:24 +0100

    Create EX2.ino

---
+++ Display de 7 segments/EX2/EX2.ino
@@ -0,0 +1,149 @@
+/**************************************************************************
+**                                                                       **
+**                                                                       **
+**                                TÍTOL:                                 **
+**                                                                       **
+**                                                                       **
+**                                                                       **
+**                                                                       **
+**NOM: Bogdan Laurentiu Cucura                            DATA:          **
+**************************************************************************/
+
+//****************************INCLUDE**************************************
+
+
+
+//****************************VARIABLE*************************************
+
+const byte a = 5;
+const byte b = 6;
+const byte c = 7;
+const byte d = 8;
+const byte e = 9;
+const byte f = 10;
+const byte g = 11;
+int temps = 1000;
+//****************************SETUP****************************************
+
+void setup() {                 //configura el final de salida 
+pinMode (a, OUTPUT);
+pinMode (b, OUTPUT);
+pinMode (c, OUTPUT);
+pinMode (d, OUTPUT);
+pinMode (e, OUTPUT);
+pinMode (f, OUTPUT);
+pinMode (g, OUTPUT);
+} 
+
+//****************************LOOP*****************************************
+void loop() {                 //configura el bucle 
+digitalWrite (a, HIGH); 
+digitalWrite (b, HIGH);
+digitalWrite (c, HIGH);
+digitalWrite (d, HIGH);
+digitalWrite (e, HIGH);
+digitalWrite (f, HIGH);
+digitalWrite (g, LOW); 
+
+delay(temps);
+
+digitalWrite (a, LOW); 
+digitalWrite (b, HIGH);
+digitalWrite (c, HIGH);
+digitalWrite (d, LOW);
+digitalWrite (e, LOW);
+digitalWrite (f, LOW);
+digitalWrite (g, LOW); 
+
+delay(temps);
+
+digitalWrite (a, HIGH); 
+digitalWrite (b, HIGH);
+digitalWrite (c, LOW);
+digitalWrite (d, HIGH);
+digitalWrite (e, HIGH);
+digitalWrite (f, LOW);
+digitalWrite (g, HIGH); 
+
+delay(temps);
+
+digitalWrite (a, HIGH); 
+digitalWrite (b, HIGH);
+digitalWrite (c, HIGH);
+digitalWrite (d, HIGH);
+digitalWrite (e, LOW);
+digitalWrite (f, LOW);
+digitalWrite (g, HIGH); 
+
+delay(temps);
+
+digitalWrite (a, LOW); 
+digitalWrite (b, HIGH);
+digitalWrite (c, HIGH);
+digitalWrite (d, LOW);
+digitalWrite (e, LOW);
+digitalWrite (f, HIGH);
+digitalWrite (g, HIGH); 
+
+delay(temps);
+
+digitalWrite (a, HIGH); 
+digitalWrite (b, LOW);
+digitalWrite (c, HIGH);
+digitalWrite (d, HIGH);
+digitalWrite (e, LOW);
+digitalWrite (f, HIGH);
+digitalWrite (g, HIGH); 
+
+delay(temps);
+
+digitalWrite (a, LOW); 
+digitalWrite (b, LOW);
+digitalWrite (c, HIGH);
+digitalWrite (d, HIGH);
+digitalWrite (e, HIGH);
+digitalWrite (f, HIGH);
+digitalWrite (g, HIGH); 
+
+delay(temps);
+
+digitalWrite (a, HIGH); 
+digitalWrite (b, HIGH);
+digitalWrite (c, HIGH);
+digitalWrite (d, LOW);
+digitalWrite (e, LOW);
+digitalWrite (f, LOW);
+digitalWrite (g, LOW); 
+
+delay(temps);
+
+digitalWrite (a, HIGH); 
+digitalWrite (b, HIGH);
+digitalWrite (c, HIGH);
+digitalWrite (d, HIGH);
+digitalWrite (e, HIGH);
+digitalWrite (f, HIGH);
+digitalWrite (g, HIGH); 
+
+delay(temps);
+
+digitalWrite (a, HIGH); 
+digitalWrite (b, HIGH);
+digitalWrite (c, HIGH);
+digitalWrite (d, LOW);
+digitalWrite (e, LOW);
+digitalWrite (f, HIGH);
+digitalWrite (g, HIGH); 
+
+delay(temps);
+
+
+
+
+
+
+
+
+}
+
+//****************************FUNCIONS*************************************
