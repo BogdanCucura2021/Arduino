a82da479a847930b6944f56b38327083ec3a286d
BogdanCucura2021 <94701310+BogdanCucura2021@users.noreply.github.com>
2022-01-10 16:09:26 +0100

    Create EX2.ino

---
+++ 001 Fade LEDs/EX2/EX2.ino
@@ -0,0 +1,203 @@
+/**************************************************************************
+**                                                                       **
+**                                                                       **
+**                                TÍTOL:                                 **
+**                                                                       **
+**                                                                       **
+**                                                                       **
+**                                                                       **
+**NOM: Bogdan Laurentiu Cucura                            DATA:          **
+**************************************************************************/
+
+//****************************INCLUDE**************************************
+
+
+
+//****************************VARIABLE*************************************
+
+const byte led0 = 3;         
+const byte led1 = 5;          
+const byte led2 = 6;          
+const byte led3 = 9;          
+const byte led4 = 10;         
+const byte led5 = 11;         
+unsigned long velocitat = 500;
+
+//****************************SETUP****************************************
+
+void setup() {                 //configura el final de salida 
+  pinMode(led0, OUTPUT);     
+  pinMode(led1, OUTPUT);     
+  pinMode(led2, OUTPUT);     
+  pinMode(led3, OUTPUT);     
+  pinMode(led4, OUTPUT);     
+  pinMode(led5, OUTPUT);
+} 
+
+//****************************LOOP*****************************************
+void loop() {                 //configura el bucle 
+ analogWrite (led0, 0;
+ analogWrite (led1, 0);
+ analogWrite (led2, 0);
+ analogWrite (led3, 0);
+ analogWrite (led4, 0);
+ analogWrite (led5, 255);
+
+ delay(velocitat);
+
+ analogWrite (led0, 0;
+ analogWrite (led1, 0);
+ analogWrite (led2, 0);
+ analogWrite (led3, 0);
+ analogWrite (led4, 255);
+ analogWrite (led5, 125);
+
+ delay(velocitat);
+
+ analogWrite (led0, 0;
+ analogWrite (led1, 0);
+ analogWrite (led2, 0);
+ analogWrite (led3, 255);
+ analogWrite (led4, 125);
+ analogWrite (led5, 60);
+
+ delay(velocitat);
+
+ analogWrite (led0, 0;
+ analogWrite (led1, 0);
+ analogWrite (led2, 255);
+ analogWrite (led3, 125);
+ analogWrite (led4, 60);
+ analogWrite (led5, 0);
+
+ delay(velocitat);
+
+ analogWrite (led0, 0;
+ analogWrite (led1, 255);
+ analogWrite (led2, 125);
+ analogWrite (led3, 60);
+ analogWrite (led4, 0);
+ analogWrite (led5, 0);
+
+ delay(velocitat);
+
+ analogWrite (led0, 255);
+ analogWrite (led1, 125);
+ analogWrite (led2, 60);
+ analogWrite (led3, 0);
+ analogWrite (led4, 0);
+ analogWrite (led5, 0);
+
+ delay(velocitat);
+
+ analogWrite (led0, 125);
+ analogWrite (led1, 60);
+ analogWrite (led2, 0);
+ analogWrite (led3, 0);
+ analogWrite (led4, 0);
+ analogWrite (led5, 0);
+
+ delay(velocitat9;
+
+ analogWrite (led0, 60);
+ analogWrite (led1, 0);
+ analogWrite (led2, 0);
+ analogWrite (led3, 0);
+ analogWrite (led4, 0);
+ analogWrite (led5, 0);
+
+ delay(velocitat);
+
+ analogWrite (led0, 0);
+ analogWrite (led1, 0);
+ analogWrite (led2, 0);
+ analogWrite (led3, 0);
+ analogWrite (led4, 0);
+ analogWrite (led5, 0);
+
+ delay(velocitat);
+
+ analogWrite (led0, 0);
+ analogWrite (led1, 0);
+ analogWrite (led2, 0);
+ analogWrite (led3, 0);
+ analogWrite (led4, 0);
+ analogWrite (led5, 0);
+
+ delay(velocitat);
+
+ analogWrite (led0, 255);
+ analogWrite (led1, 0);
+ analogWrite (led2, 0);
+ analogWrite (led3, 0);
+ analogWrite (led4, 0);
+ analogWrite (led5, 0);
+
+ delay(velocitat);
+
+ analogWrite (led0, 125);
+ analogWrite (led1, 255);
+ analogWrite (led2, 0);
+ analogWrite (led3, 0);
+ analogWrite (led4, 0);
+ analogWrite (led5, 0);
+ 
+ delay(velocitat);
+
+ analogWrite (led0, 60);
+ analogWrite (led1, 125);
+ analogWrite (led2, 255);
+ analogWrite (led3, 0);
+ analogWrite (led4, 0);
+ analogWrite (led5, 0);
+
+ delay(velocitat);
+
+ analogWrite (led0, 0);
+ analogWrite (led1, 60);
+ analogWrite (led2, 125);
+ analogWrite (led3, 255);
+ analogWrite (led4, 0);
+ analogWrite (led5, 0);
+
+ delay(velocitat);
+
+ analogWrite (led0, 0);
+ analogWrite (led1, 0);
+ analogWrite (led2, 60);
+ analogWrite (led3, 125);
+ analogWrite (led4, 255);
+ analogWrite (led5, 0);
+
+ delay(velocitat);
+
+ analogWrite (led0, 0);
+ analogWrite (led1, 0);
+ analogWrite (led2, 0);
+ analogWrite (led3, 60);
+ analogWrite (led4, 125);
+ analogWrite (led5, 255);
+
+ delay(velocitat);
+
+ analogWrite (led0, 0);
+ analogWrite (led1, 0);
+ analogWrite (led2, 0);
+ analogWrite (led3, 0);
+ analogWrite (led4, 60);
+ analogWrite (led5, 125);
+
+ delay(velocitat);
+
+ analogWrite (led0, 0);
+ analogWrite (led1, 0);
+ analogWrite (led2, 0);
+ analogWrite (led3, 0);
+ analogWrite (led4, 0);
+ analogWrite (led5, 60);
+
+
+
+}
+
+//****************************FUNCIONS*************************************
