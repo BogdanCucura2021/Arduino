8dbe54e72a83fe9040f708a297c6f9b4fdba87a2
BogdanCucura2021 <94701310+BogdanCucura2021@users.noreply.github.com>
2022-01-17 17:06:40 +0100

    Update Programa_final.ino

--- Sensors i actuadors/Programa_final/Programa_final.ino
+++ Sensors i actuadors/Programa_final/Programa_final.ino
@@ -11,104 +11,65 @@
 **************************************************************************/
 
 //****************************INCLUDE**************************************
-
 #include <IRremote.h>
 #include <IRremoteInt.h>
-
-#define BLUE 3
-#define GREEN 5
-#define RED 6
-#include "IRremote.h"
-
-
-
 //****************************VARIABLE*************************************
 int receiver = 11; 
-int redValue;
-int greenValue;
-int blueValue;
+int redPin = 6;
+int greenPin = 5;
+int bluePin = 3;
 int valor;
 IRrecv irrecv(receiver);     
 decode_results results;
 
 //****************************SETUP****************************************
 
-void setup() {                 //configura el final de salida 
+void setup()                  //configura el final de salida 
 {
-pinMode(RED, OUTPUT);
-pinMode(GREEN, OUTPUT);
-pinMode(BLUE, OUTPUT);
-digitalWrite(RED, HIGH);
-digitalWrite(GREEN, LOW);
-digitalWrite(BLUE, LOW);
-
-}
-  Serial.begin(9600);
-  Serial.println("IR Receiver Button Decode"); 
-  irrecv.enableIRIn(); // Start the receiver
-
+pinMode(redPin, OUTPUT);
+pinMode(greenPin, OUTPUT);
+pinMode(bluePin, OUTPUT); 
+pinMode (receiver, INPUT);  
+Serial.begin(9600);
+Serial.println("IR Receiver Button Decode"); 
+irrecv.enableIRIn(); 
 }
 
 
 //****************************LOOP*****************************************
-void loop() {                 //configura el bucle 
+void loop()
+
+{                 //configura el bucle 
+if (irrecv.decode(&results)){ // have we received an IR signal?
+ 
+Serial.print (results.value, HEX);
+  
+ if (results.value == 0xFF30CF) { 
+
+analogWrite (redPin, 0);
+analogWrite (greenPin, 255);
+analogWrite (bluePin, 0); 
+}
+if (results.value == 0xFF18E7)
 {
-  if (irrecv.decode(&results)) // have we received an IR signal?
-
-  {
-    translateIR(); 
-    irrecv.resume(); // receive the next value
-  }  
+analogWrite (redPin, 255);
+analogWrite (greenPin, 0);
+analogWrite (bluePin, 0);
 }
-switch (results.value)
+if (results.value == 0xFF7A85)
 {
-  case 0xFF30CF: Serial.println("1");
-redValue = 255; 
-greenValue = 0;
-blueValue = 0;
-
-  break;  
+analogWrite (redPin, 0);
+analogWrite (greenPin, 0);
+analogWrite (bluePin, 255);
 }
 
+
+irrecv.resume();
+ 
 }
+}
+
+
+
 
 //****************************FUNCIONS*************************************
-void translateIR() // takes action based on IR code received
-
-// describing Remote IR codes 
-
-{
-
-  switch(results.value)
-
-  {
-  case 0xFFA25D: Serial.println("POWER"); break;
-  case 0xFFE21D: Serial.println("FUNC/STOP"); break;
-  case 0xFF629D: Serial.println("VOL+"); break;
-  case 0xFF22DD: Serial.println("FAST BACK");    break;
-  case 0xFF02FD: Serial.println("PAUSE");    break;
-  case 0xFFC23D: Serial.println("FAST FORWARD");   break;
-  case 0xFFE01F: Serial.println("DOWN");    break;
-  case 0xFFA857: Serial.println("VOL-");    break;
-  case 0xFF906F: Serial.println("UP");    break;
-  case 0xFF9867: Serial.println("EQ");    break;
-  case 0xFFB04F: Serial.println("ST/REPT");    break;
-  case 0xFF6897: Serial.println("0");    break;
-  case 0xFF30CF: Serial.println("1");    break;   
-  case 0xFF18E7: Serial.println("2");    break;
-  case 0xFF7A85: Serial.println("3");    break;
-  case 0xFF10EF: Serial.println("4");    break;
-  case 0xFF38C7: Serial.println("5");    break;
-  case 0xFF5AA5: Serial.println("6");    break;
-  case 0xFF42BD: Serial.println("7");    break;
-  case 0xFF4AB5: Serial.println("8");    break;
-  case 0xFF52AD: Serial.println("9");    break;
-  case 0xFFFFFFFF: Serial.println(" REPEAT");break;  
-
-  default: 
-    Serial.println(" other button   ");
-
-  }// End Case
-
-  delay(500);
-}
