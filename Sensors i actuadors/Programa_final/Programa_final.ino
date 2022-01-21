0165b812a3f8f48c91b3abba02017dd16478cadc
BogdanCucura2021 <94701310+BogdanCucura2021@users.noreply.github.com>
2022-01-21 12:55:37 +0100

    Update Programa_final.ino

--- Sensors i actuadors/Programa_final/Programa_final.ino
+++ Sensors i actuadors/Programa_final/Programa_final.ino
@@ -18,7 +18,10 @@ int receiver = 11;
 int redPin = 6;
 int greenPin = 5;
 int bluePin = 3;
-int delayTime = 200;
+int delayTime = 5;
+int redValue;
+int greenValue;
+int blueValue;
 IRrecv irrecv(receiver);     
 decode_results results;
 
@@ -40,9 +43,16 @@ irrecv.enableIRIn();
 void loop()
 
 {                 //configura el bucle 
- 
+if (irrecv.decode(&results))
+{  
 switch(results.value) 
 {
+case 0xFFA25D:
+analogWrite (redPin, 0);
+analogWrite (greenPin, 0);
+analogWrite (bluePin, 0);
+break;
+  
 case 0xFF6897: //0
 analogWrite (redPin, 0);
 analogWrite (greenPin, 255);
@@ -53,14 +63,14 @@ case 0xFF30CF: //1
  
 analogWrite (redPin, 255);
 analogWrite (greenPin, 0);
-analogWrite (bluePin, 0);
+analogWrite (bluePin, 255);
 break;
 
 case 0xFF18E7: //2
 
-analogWrite (redPin, 0);
+analogWrite (redPin, 255);
 analogWrite (greenPin, 0);
-analogWrite (bluePin, 255);
+analogWrite (bluePin, 0);
 break;
 
 case 0xFF7A85: //3
@@ -71,10 +81,48 @@ analogWrite (bluePin, 255);
 break;
 
 case 0xFFE21D: // Func/Stop
+redValue = 255; 
+greenValue = 0;
+blueValue = 0;
 
+for(int i = 0; i < 255; i += 1) 
+{
+redValue -= 1;
+greenValue += 1;
 
-irrecv.resume();
- 
+analogWrite(redPin, redValue);
+analogWrite(greenPin, greenValue);
+delay(delayTime);
+}
+
+redValue = 0;
+greenValue = 255;
+blueValue = 0;
+
+for(int i = 0; i < 255; i += 1) 
+{
+greenValue -= 1;
+blueValue += 1;
+analogWrite(greenPin, greenValue);
+analogWrite(bluePin, blueValue);
+delay(delayTime);
+}
+
+redValue = 0;
+greenValue = 0;
+blueValue = 255;
+
+for(int i = 0; i < 255; i += 1) 
+{
+blueValue -= 1;
+redValue += 1;
+analogWrite(bluePin, blueValue);
+analogWrite(redPin, redValue);
+delay(delayTime);
+break;
+}
+}
+ irrecv.resume();
 }
 }
 
